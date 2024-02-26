68ac6af571c0394ec37174ec2d25264e8ccebe1f
Alexandre <alexandrepereiraassano@gmail.com>
2024-02-26 16:32:45 +0900

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 require("dotenv").config();
 
-console.log(process.env.NODE_ENV);
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use("/public", express.static(path.join(__dirname, "/public")));
+
+app.use("/", require("./routes/index.js"));
+
+app.listen(PORT, _ => {
+  console.log(`Application listening at http://127.0.0.1:${PORT}`);
+});
