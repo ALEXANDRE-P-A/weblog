e04fab391c80dd984e0f65ff1ec8aa440fc8ad3d
Alexandre <alexandrepereiraassano@gmail.com>
2024-02-26 16:52:06 +0900

    third commit

--- app.js
+++ app.js
@@ -3,15 +3,30 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 
+const logger = require("./lib/log/logger.js");
+const applicationlogger = require("./lib/log/applicationlogger.js");
+const accesslogger = require("./lib/log/accesslogger.js");
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// set view engine
 app.set("view engine", "ejs");
+
+// static resources
 app.use("/public", express.static(path.join(__dirname, "/public")));
 
+// set accesslogger
+app.use(accesslogger());
+
+// set routes
 app.use("/", require("./routes/index.js"));
 
+// set application logger
+app.use(applicationlogger());
+
+// application listen
 app.listen(PORT, _ => {
-  console.log(`Application listening at http://127.0.0.1:${PORT}`);
+  logger.application.info(`Application listening at http://127.0.0.1:${PORT}`);
 });
